0c567d791a38881fd02334ce9e5f2a199cf10e9b
yuki-sato-giotech <yuki_sato@gio-tech.jp>
2023-10-09 11:01:40 +0900

    Separate configuration files to be loaded for each build schema

---
+++ ICE/ICE/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  ICE
+//
+//  Created by 佐藤友貴 on 2023/10/09.
+//
+
+import Foundation
---
+++ ICE/ICE/Enum/Error.swift
@@ -0,0 +1,8 @@
+//
+//  Error.swift
+//  ICE
+//
+//  Created by 佐藤友貴 on 2023/10/09.
+//
+
+import Foundation
