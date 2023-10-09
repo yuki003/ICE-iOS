4122d772b1227bd49ef762e68fb156121a023be0
yuki-sato-giotech <yuki_sato@gio-tech.jp>
2023-10-09 11:02:10 +0900

    Separate configuration files to be loaded for each build schema

--- ICE/ICE/AppDelegate.swift
+++ ICE/ICE/AppDelegate.swift
@@ -5,4 +5,40 @@
 //  Created by 佐藤友貴 on 2023/10/09.
 //
 
+import UIKit
+import Amplify
 import Foundation
+import AWSCognitoAuthPlugin
+import AWSAPIPlugin
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        do {
+            try configureAmplify()
+        } catch {
+            print("Error configuring Amplify: \(error)")
+        }
+        return true
+    }
+
+    private func configureAmplify() throws {
+        let configFile = Bundle.main.object(forInfoDictionaryKey: "amplify_configration_file")
+        guard let url = Bundle.main.url(forResource: configFile as? String, withExtension: "json") else {
+            throw ConfigurationError.missingConfig
+        }
+        let amplifyConfig = try AmplifyConfiguration(configurationFile: url)
+            try Amplify.configure(amplifyConfig)
+        try Amplify.add(plugin: AWSCognitoAuthPlugin())
+//        try Amplify.add(plugin: AWSAPIPlugin(modelRegistration: AmplifyModels()))
+        try Amplify.configure()
+        print("Amplify configured with auth and api plugin")
+    }
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+}
--- ICE/ICE/Enum/Error.swift
+++ ICE/ICE/Enum/Error.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+enum ConfigurationError: Error {
+    case missingConfig
+}
