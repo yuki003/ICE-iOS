443de83dad2837135f8b40e6a2879af922595688
yuki-sato-giotech <yuki_sato@gio-tech.jp>
2023-10-14 16:52:42 +0900

    amplify delete

--- ICE/ICE/AmplifyModels/AccountType.swift
+++
@@ -1,9 +0,0 @@
-// swiftlint:disable all
-import Amplify
-import Foundation
-
-public enum AccountType: String, EnumPersistable {
-  case guest = "GUEST"
-  case host = "HOST"
-  case dev = "DEV"
-}
--- ICE/ICE/AmplifyModels/Group.swift
+++
@@ -1,9 +0,0 @@
-// swiftlint:disable all
-import Amplify
-import Foundation
-
-public struct Group: Embeddable {
-  var groupID: String
-  var name: String
-  var userIDs: [String?]
-}
